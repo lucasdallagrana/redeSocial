af41ebe9fbfcf047e236f36b0338f4221666e645
lucasdallagrana <lucas.dallagrana@escola.pr.gov.br>
2026-08-06 12:14:15 -0300

    ok

---
+++ script.js
@@ -0,0 +1,7 @@
+//botao curtida
+
+document.addEventListener("DOMContentLoaded", () => {
+    const likeBtn = document.querySelector(".left-actions .action-btn:first-child");
+    if (!likeBtn) return;
+    const likeSvg =likeBtn.querySelector("svg");
+}
