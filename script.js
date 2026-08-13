70bb17674d741e55f4ff6f9661e656f543a14169
lucasdallagrana <lucas.dallagrana@escola.pr.gov.br>
2026-08-13 12:14:38 -0300

    ok

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const likeBtn = document.querySelector(".left-actions .action-btn:first-child");
     if (!likeBtn) return;
     const likeSvg =likeBtn.querySelector("svg");
+
+   
+    //localiza o contador
+
+    let textNode = Array.from(likeBtn.childNodes).find(node) =>
 }
