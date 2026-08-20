83359b70b74d2b43832483cbe6f23b3af97a8743
lucasdallagrana <lucas.dallagrana@escola.pr.gov.br>
2026-08-20 12:19:00 -0300

    ok

--- script.js
+++ script.js
@@ -5,8 +5,31 @@ document.addEventListener("DOMContentLoaded", () => {
     if (!likeBtn) return;
     const likeSvg =likeBtn.querySelector("svg");
 
-   
+
     //localiza o contador
 
     let textNode = Array.from(likeBtn.childNodes).find(node) => node.nodeType
+    ===node.TEXT-NODE && node.textContent.trim() !== ""
+)
+
+//zera o contador
+let cont = 0;
+
+//atualiza
+if(textNode){
+textNode.textContent = '0';
+}
+
+//coração
+function applyLikedStyle (){
+likeSvg.style.fill = "#ef4444";
+likeSvg.style.stroke = "#ef4444";
+likeSvg.style.color = "#ef4444";
+}
+
+//efito curtida
+likeSvg.style.transform = "scale(1.3)";
+setTimeout (() => (likeSvg.style.transform = "scal(1)")150);
+
+
 }
